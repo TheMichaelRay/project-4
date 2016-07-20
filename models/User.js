e578987de9d54af30d25060ca950af5eeae97fae
Michael Ray <raymichael23@gmail.com>
2016-07-19 18:16:58 -0700

    added user model

---
+++ models/User.js
@@ -0,0 +1,30 @@
+var
+  mongoose = require('mongoose'),
+  bcrypt = require('bcrypt-nodejs'),
+  Schema = mongoose.Schema,
+  userSchema = new Schema ({
+    local: {
+      email: {type: String, required: true, unique: true},
+      password: {type: String, required: true}
+    },
+    firstName: {type: String, required: true},
+    lastName: {type: String, required: true},
+    avatar: String,
+    bio: String,
+    age: {type: String, required: true, min: 18}
+  },
+  {
+    timestamps: true
+  })
+
+userSchema.methods.generateHash = function (password) {
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(8))
+}
+
+userSchema.methods.validPassword = function (password) {
+  return bcrypt.compareSync(password, this.local.password)
+}
+
+var User = mongoose.model('User', userSchema)
+
+module.exports = User
